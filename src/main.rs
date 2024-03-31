3329959c72fed961eb6e5cda03df6cb244edce56
imkiva <imkiva@islovely.icu>
2024-03-31 13:05:24 +0800

    project: dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use log::{info, warn};
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
+    match dotenvy::dotenv() {
+        Err(e) => warn!("dotenv(): failed to load .env file: {}", e),
+        _ => {}
+    }
+
+    info!("Hello, world!");
 }
