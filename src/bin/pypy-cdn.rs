98e6ffe62423b2461d395bba452ff1e1de1584fc
imkiva <imkiva@islovely.icu>
2024-04-15 15:49:03 +0800

    misc: fix env loading

--- src/bin/pypy-cdn.rs
+++ src/bin/pypy-cdn.rs
@@ -14,13 +14,14 @@ use warp_real_ip::get_forwarded_for;
 
 #[tokio::main]
 async fn main() {
-    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info"))
-        .filter(Some("warp::server"), log::LevelFilter::Off)
-        .init();
     match dotenvy::dotenv() {
         Err(e) => warn!("dotenv(): failed to load .env file: {}", e),
         _ => {}
     }
+
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info"))
+        .filter(Some("warp::server"), log::LevelFilter::Off)
+        .init();
 
     let opts = AppOpts::parse();
 
