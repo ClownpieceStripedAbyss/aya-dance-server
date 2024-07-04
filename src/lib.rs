06b1c6380561ae323571580829ed88fa8f70214b
imkiva <imkiva@islovely.icu>
2024-07-05 01:55:53 +0800

    cli: add `builtin_l3_forward_ud` for `api.udon.dance`

--- src/lib.rs
+++ src/lib.rs
@@ -39,6 +39,8 @@ pub struct AppOpts {
   pub builtin_l3_listen: Option<String>,
   #[clap(short = 'f', long, env, default_value = "jd-orig.kiva.moe:443")]
   pub builtin_l3_forward: String,
+  #[clap(short = 'f', long, env, default_value = "ud-orig.kiva.moe:443")]
+  pub builtin_l3_forward_ud: String,
   #[clap(short = 'w', long, env, default_value = "0.0.0.0:7991")]
   pub rtsp_listen: String,
   #[clap(long, env, default_value = "5")]
