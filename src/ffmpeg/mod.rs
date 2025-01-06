9d769d6e11a35370cab85ddf20fc4effd0b7318d
imkiva <imkiva@islovely.icu>
2025-01-06 16:29:24 +0800

    fix: double free

--- src/ffmpeg/mod.rs
+++ src/ffmpeg/mod.rs
@@ -1,5 +1,5 @@
 use std::{ffi::CString, ptr, ptr::NonNull};
-
+use std::mem::ManuallyDrop;
 use anyhow::anyhow;
 use rsmpeg::{
   avcodec::AVCodecParameters,
@@ -7,7 +7,7 @@ use rsmpeg::{
   avutil, ffi,
 };
 
-fn ffmpeg_audio_test() -> anyhow::Result<()> {
+pub fn ffmpeg_audio_test() -> anyhow::Result<()> {
   let input_file = CString::new("input.mp4")?;
   let output_file = CString::new("output.mp4")?;
   let audio_offset = -0.16667; // Audio delay in seconds
@@ -47,15 +47,16 @@ fn ffmpeg_audio_test() -> anyhow::Result<()> {
     AVCodecParameters::from_raw(NonNull::new(video_in_stream.codecpar).unwrap())
   });
 
-  // Add audio stream to output
-  let mut audio_out_stream = unsafe {
+  // Add video stream to output, NOTE: wrap it in ManuallyDrop to prevent drop,
+  // since the video_out_stream will drop the same pointer, which will cause double free.
+  let mut audio_out_stream = ManuallyDrop::new(unsafe {
     let new_stream = NonNull::new(ffi::avformat_new_stream(
       output_ctx.as_mut_ptr(),
       ptr::null(),
     ))
     .ok_or_else(|| anyhow!("Failed to call avformat_new_stream"))?;
     AVStreamMut::from_raw(new_stream)
-  };
+  });
   audio_out_stream.set_time_base(audio_in_stream.time_base);
   audio_out_stream.set_codecpar(unsafe {
     AVCodecParameters::from_raw(NonNull::new(audio_in_stream.codecpar).unwrap())
@@ -75,16 +76,16 @@ fn ffmpeg_audio_test() -> anyhow::Result<()> {
     } else if stream_index == audio_in_stream_index {
       // Audio stream, adjust timestamp for offset
       packet.set_pts(avutil::av_rescale_q(
-        packet.pts + (audio_offset * time_base_to_double(audio_in_stream.time_base) as f64) as i64,
+        packet.pts + (audio_offset * time_base_to_double(audio_in_stream.time_base)) as i64,
         audio_in_stream.time_base,
         audio_out_stream.time_base,
       ));
       packet.set_dts(avutil::av_rescale_q(
-        packet.dts + (audio_offset * time_base_to_double(audio_in_stream.time_base) as f64) as i64,
+        packet.dts + (audio_offset * time_base_to_double(audio_in_stream.time_base)) as i64,
         audio_in_stream.time_base,
         audio_out_stream.time_base,
       ));
-
+      
       packet.set_stream_index(audio_out_stream.index as i32);
       output_ctx.write_frame(&mut packet)?;
     }
