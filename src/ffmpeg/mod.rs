a33d2e8a7a9cd35754780881f77aa76a9c5c5469
imkiva <imkiva@islovely.icu>
2025-01-06 19:20:47 +0800

    fix: memory

--- src/ffmpeg/mod.rs
+++ src/ffmpeg/mod.rs
@@ -1,11 +1,10 @@
-use std::{ffi::CString, ptr, ptr::NonNull};
-use std::mem::ManuallyDrop;
+use std::ffi::CString;
 use anyhow::anyhow;
-use rsmpeg::{avcodec::AVCodecParameters, avformat::{AVFormatContextInput, AVFormatContextOutput, AVStreamMut}, avutil, ffi, UnsafeDerefMut};
+use rsmpeg::{avformat::{AVFormatContextInput, AVFormatContextOutput}, avutil, ffi};
 
-pub fn ffmpeg_audio_test() -> anyhow::Result<()> {
-  let input_file = CString::new("input.mp4")?;
-  let audio_offset = -0.16667; // Audio delay in seconds
+pub fn ffmpeg_audio_compensation(input_file: &str, output_file: &str, audio_offset: f64) -> anyhow::Result<()> {
+  let input_file = CString::new(input_file)?;
+  let output_file = CString::new(output_file)?;
 
   // Open input file
   let mut input_ctx = AVFormatContextInput::open(&input_file, None, &mut None)?;
@@ -22,44 +21,33 @@ pub fn ffmpeg_audio_test() -> anyhow::Result<()> {
     .position(|stream| stream.codecpar().codec_type == rsmpeg::ffi::AVMEDIA_TYPE_AUDIO)
     .ok_or_else(|| anyhow!("No audio stream found"))?;
 
-  let video_in_stream = input_ctx.streams()[video_in_stream_index].clone();
-  let audio_in_stream = input_ctx.streams()[audio_in_stream_index].clone();
+  let video_in_stream = &input_ctx.streams()[video_in_stream_index];
+  let audio_in_stream = &input_ctx.streams()[audio_in_stream_index];
 
   // Create output context with in-memory IO
-  let output_file = CString::new("output.mkv")?;
   let mut output_ctx = AVFormatContextOutput::create(&output_file, None)?;
 
   // Add video stream to output
-  let mut video_out_stream = unsafe {
-    let new_stream = NonNull::new(ffi::avformat_new_stream(
-      output_ctx.as_mut_ptr(),
-      ptr::null(),
-    ))
-    .ok_or_else(|| anyhow!("Failed to call avformat_new_stream"))?;
-    AVStreamMut::from_raw(new_stream)
-  };
-  video_out_stream.set_time_base(video_in_stream.time_base);
-  video_out_stream.set_codecpar(unsafe {
-    AVCodecParameters::from_raw(NonNull::new(video_in_stream.codecpar).unwrap())
-  });
+  {
+    let mut video_out_stream = output_ctx.new_stream();
+    video_out_stream.set_time_base(video_in_stream.time_base);
+    video_out_stream.set_codecpar(video_in_stream.codecpar().clone());
+  }
 
-  // Add audio stream to output, NOTE: wrap it in ManuallyDrop to prevent drop,
-  // since the video_out_stream will drop the same pointer, which will cause double free.
-  let mut audio_out_stream = ManuallyDrop::new(unsafe {
-    let new_stream = NonNull::new(ffi::avformat_new_stream(
-      output_ctx.as_mut_ptr(),
-      ptr::null(),
-    ))
-    .ok_or_else(|| anyhow!("Failed to call avformat_new_stream"))?;
-    AVStreamMut::from_raw(new_stream)
-  });
-  audio_out_stream.set_time_base(audio_in_stream.time_base);
-  audio_out_stream.set_codecpar(unsafe {
-    let mut c = AVCodecParameters::from_raw(NonNull::new(audio_in_stream.codecpar).unwrap());
-    c.deref_mut().codec_id = ffi::AV_CODEC_ID_AAC;
-    c.deref_mut().codec_tag = 0;
-    c
-  });
+  // Add audio stream to output
+  {
+    let mut audio_out_stream = output_ctx.new_stream();
+    audio_out_stream.set_time_base(audio_in_stream.time_base);
+    audio_out_stream.set_codecpar(audio_in_stream.codecpar().clone());
+    
+    // audio_out_stream.set_codecpar(unsafe {
+    //   // let mut c = AVCodecParameters::from_raw(NonNull::new(audio_in_stream.codecpar).unwrap());
+    //   // c.deref_mut().codec_id = ffi::AV_CODEC_ID_AAC;
+    //   // c.deref_mut().codec_tag = 0;
+    //   // c
+    //   AVCodecParameters::from_raw(NonNull::new(audio_in_stream.codecpar).unwrap()).clone()
+    // }); 
+  }
 
   // Open output file
   output_ctx.write_header(&mut None)?;
@@ -67,22 +55,28 @@ pub fn ffmpeg_audio_test() -> anyhow::Result<()> {
   // Read packets from input and write to output
   while let Some(mut packet) = input_ctx.read_packet()? {
     let stream_index = packet.stream_index as usize;
-    let out_stream: &AVStreamMut;
+    let out_stream_time_base;
+    let out_stream_index;
     let in_stream = &input_ctx.streams()[stream_index];
 
     if stream_index == video_in_stream_index {
-      out_stream = &mut video_out_stream;
+      let x = output_ctx.streams().iter().find(|s| s.codecpar().codec_type == rsmpeg::ffi::AVMEDIA_TYPE_VIDEO).unwrap();
+      out_stream_time_base = x.time_base;
+      out_stream_index = x.index;
     } else if stream_index == audio_in_stream_index {
-      out_stream = &mut audio_out_stream;
+      let x = output_ctx.streams().iter().find(|s| s.codecpar().codec_type == rsmpeg::ffi::AVMEDIA_TYPE_AUDIO).unwrap();
+      out_stream_time_base = x.time_base;
+      out_stream_index = x.index;
+      
       packet.set_pts(packet.pts + avutil::av_rescale_q(
         (audio_offset * ffi::AV_TIME_BASE as f64) as i64,
         ffi::AV_TIME_BASE_Q,
-        out_stream.time_base,
+        out_stream_time_base,
       ));
       packet.set_dts(packet.dts + avutil::av_rescale_q(
         (audio_offset * ffi::AV_TIME_BASE as f64) as i64,
         ffi::AV_TIME_BASE_Q,
-        out_stream.time_base,
+        out_stream_time_base,
       ));
     } else {
       continue;
@@ -91,21 +85,21 @@ pub fn ffmpeg_audio_test() -> anyhow::Result<()> {
     packet.set_pts(avutil::av_rescale_q_rnd(
       packet.pts,
       in_stream.time_base,
-      out_stream.time_base,
+      out_stream_time_base,
       ffi::AV_ROUND_NEAR_INF as u32,
     ));
     packet.set_dts(avutil::av_rescale_q_rnd(
       packet.dts,
       in_stream.time_base,
-      out_stream.time_base,
+      out_stream_time_base,
       ffi::AV_ROUND_NEAR_INF as u32,
     ));
     packet.set_duration(avutil::av_rescale_q(
       packet.duration,
       in_stream.time_base,
-      out_stream.time_base,
+      out_stream_time_base,
     ));
-    packet.set_stream_index(out_stream.index as i32);
+    packet.set_stream_index(out_stream_index as i32);
 
     output_ctx.interleaved_write_frame(&mut packet)?;
   }
